c87d7ecda6c103ec14609f035cad406a2ff19e34
Atlapaltin <iksanov@list.ru>
2022-10-02 16:05:57 +0300

    Line of code removed

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -6,7 +6,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(R.string.nmedia)
         println(getString(R.string.nmedia))
     }
 }
